c4526ddb8dba7fd26b6618ca7d75a7565a54d8fb
itmr1212 <mari1212hm6@gmail.com>
2020-12-08 16:20:58 +0900

    ユーザー管理機能 修正1回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :first_name1, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name1, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name2, presence: true, format: {with: /\A([ァ-ン]|ー)+$+\z/}
-  validates :last_name2, presence: true, format: {with: /\A([ァ-ン]|ー)+$+\z/}
-  validates :birthday, presence: true
-  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
+  with_options presence: true ,format: {with: /\A([ァ-ン]|ー)+$+\z/} do
+  validates :first_name2
+  validates :last_name2
+  end
   
+  with_options presence: true ,format: {with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :first_name1
+    validates :last_name1
+  end
+
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
 end
